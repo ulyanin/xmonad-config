f67c381e13bdf089a6758815216f9952fb2c0886
Ulyanin Dmitry <ulyanin1997@gmail.com>
2017-07-24 10:19:27 +0300

    xmonad base

---
+++ xmobar.hs
@@ -0,0 +1,43 @@
+Config { 
+    font = "xft:Dejavu Sans Mono:size=11:regular:antialias=true"
+    bgColor = "#000000",
+    fgColor = "#ffffff",
+    position = Static { xpos = 0, ypos = 0, width = 1920, height = 20 },
+    lowerOnStart = True,
+    commands = [
+         Run Weather "UUDD" ["-t","<tempC>°C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
+        ,Run Memory ["-t","<used>/<total>M (<cache>M)","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10        
+        ,Run Network "wlp2s0" [
+             "-t"    ,"rx:<rx>, tx:<tx>"
+            ,"-H"   ,"200"
+            ,"-L"   ,"10"
+            ,"-h"   ,"#FFB6B0"
+            ,"-l"   ,"#CEFFAC"
+            ,"-n"   ,"#FFFFCC"
+            , "-c"  , " "
+            , "-w"  , "2"
+            ] 10
+        ,Run Date "%d.%m.%Y %H:%M:%S" "date" 10
+        ,Run MultiCpu [ "--template" , "<autototal>"
+            , "--Low"      , "50"         -- units: %
+            , "--High"     , "85"         -- units: %
+            , "--low"      , "gray"
+            , "--normal"   , "darkorange"
+            , "--high"     , "darkred"
+            , "-c"         , " "
+            , "-w"         , "3"
+        ] 10
+        ,Run CoreTemp [ "--template" , "<core0> <core1> <core2>°C"
+            , "--Low"      , "70"        -- units: °C
+            , "--High"     , "80"        -- units: °C
+            , "--low"      , "darkgreen"
+            , "--normal"   , "darkorange"
+            , "--high"     , "darkred"
+        ] 50
+        ,Run StdinReader
+        ,Run PipeReader "/tmp/.volume-pipe" "vol"
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %coretemp% | %multicpu% | %memory%  | %wlp2s0% | %UUDD% | <fc=#FFFFCC>%date%</fc>   "
+}
---
+++ xmonad.hs
@@ -0,0 +1,75 @@
+import XMonad
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.Spacing
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Gaps
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Mosaic
+import XMonad.Layout.Tabbed
+import XMonad.Layout.DwmStyle
+import XMonad.Layout.Tabbed
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import System.IO (hPutStrLn)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Actions.CycleWS
+
+xmobarTitleColor :: String
+xmobarTitleColor = "#FFB6B0"
+xmobarCurrentWorkspaceColor = "#CEFFAC"
+
+defaults = defaultConfig {
+          terminal                 = "konsole"
+        , workspaces               = myWorkspaces
+        , modMask                  = mod4Mask
+        , layoutHook               = myLayoutHook
+        , handleEventHook          = fullscreenEventHook  -- для корректного отображения окон в полно экранном режиме
+        , startupHook              = setWMName "LG3D"  -- для совместимости определёных приложений, java например(IntelliJ IDEA)
+        , borderWidth              = 2
+        , normalBorderColor        = "black"
+        , focusedBorderColor       = "orange"
+        } `additionalKeys` myKeys  -- добавили к дефолтной конфигурации новые кнпоки!
+
+altMask = mod1Mask
+
+myKeys = [ ((altMask .|. controlMask, xK_l), spawn "slock")
+         , ((altMask .|. controlMask, xK_Left  ), prevWS)
+         , ((altMask .|. controlMask, xK_Right ), nextWS)
+         ]
+
+myWorkspaces :: [String]
+myWorkspaces =  ["1:web","2:dev","3:term","4:vm","5:media"] ++ map show [6..9]
+
+myTabConfig = def { inactiveBorderColor = "#FF0000"
+                  , activeTextColor = "#00FF00"}
+
+myLayoutHook = spacing 6 $ gaps [(U, 20)] $ toggleLayouts (noBorders Full) $
+    smartBorders $ Mirror tiled ||| mosaic 2 [3, 2]  ||| tabbed shrinkText myTabConfig
+      where 
+        tiled = Tall nmaster delta ratio
+        nmaster = 1
+        delta   = 3/100
+        ratio   = 3/5
+
+
+main = do
+	xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar.hs"
+	xmonad $ defaults {
+	logHook =  dynamicLogWithPP $ defaultPP {
+            ppOutput = System.IO.hPutStrLn xmproc
+          , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+          , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor "" . wrap "[" "]"
+          , ppSep = "   "
+          , ppWsSep = " "
+          , ppLayout  = (\ x -> case x of
+              "Spacing 6 Mosaic"                      -> "[:]"
+              "Spacing 6 Mirror Tall"                 -> "[M]"
+              "Spacing 6 Hinted Tabbed Simplest"      -> "[T]"
+              "Spacing 6 Full"                        -> "[ ]"
+              _                                       -> x )
+          , ppHiddenNoWindows = showNamedWorkspaces
+      } 
+} where showNamedWorkspaces wsId = if any (`elem` wsId) ['a'..'z']
+                                       then pad wsId
+                                       else ""
